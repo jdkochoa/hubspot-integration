32fea01f6ddd71358882245949c5a09f98331104
jdkochoa <102246850+jdkochoa@users.noreply.github.com>
2023-06-10 23:20:03 +0000

    "commit"

--- src/main/java/com/fiftyfloor/ControllerTester.java
+++ src/main/java/com/fiftyfloor/ControllerTester.java
@@ -1,5 +1,14 @@
 
 @MicronautTest
-public class ControllerTester {
-    
+public class JobLeadControllerClient {
+     
+@Inject
+EmbeddedServer server;
+
+@Inject
+@Client("/")
+HttpClient client;
+
+@Test
+void test hubspo
 }
